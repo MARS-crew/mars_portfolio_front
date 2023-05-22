726dbfcfc83210b1db74c8b5179f50331acd5d15
HoYeon Jo <akftjd100@gmail.com>
2023-05-22 23:16:49 +0900

    [생성] 포트폴리오 소개 페이지

--- views/screens/Login.js
+++ views/screens/Login.js
@@ -1,32 +1,23 @@
 import React from 'react';
-import {
-StyleSheet,
-View,
-} from 'react-native';
+import {StyleSheet, Button, View} from 'react-native';
 import Input from '../components/Input';
 
-
-
-const Login= () => {
-return(
+const Login = ({navigation}) => {
+  return (
     <View style={styles.container}>
-        <Input
-        inputType={'id'}
-        placeholder={'아이디'}
-        />
-        <Input
-        inputType={'password'}
-        placeholder={'비밀번호'}
-        />
+      <Input inputType={'id'} placeholder={'아이디'} />
+      <Input inputType={'password'} placeholder={'비밀번호'} />
+      <Button
+        title="로그인"
+        onPress={() => navigation.navigate('Portfolio')}></Button>
     </View>
-)
+  );
 };
 
 const styles = StyleSheet.create({
-    container:{
-    backgroundColor: 'white'
-    }
+  container: {
+    backgroundColor: 'white',
+  },
 });
-
 
 export default Login;
