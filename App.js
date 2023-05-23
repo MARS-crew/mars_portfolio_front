81c915737af0275db915c8e1bbf423cdaefaf0f5
HoYeon Jo <akftjd100@gmail.com>
2023-05-23 20:50:34 +0900

    Revert "[생성] 포트폴리오 소개 페이지"

--- App.js
+++ App.js
@@ -1,30 +1,24 @@
-import 'react-native-gesture-handler';
+import "react-native-gesture-handler"
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
+import { NavigationContainer } from "@react-navigation/native"
+import { createStackNavigator } from "@react-navigation/stack";
 
-import Splash from './views/screens/splash';
-import Main from './views/screens/Main';
-import Login from './views/screens/Login';
-import Portfolio from './views/screens/Portfolio';
+import Splash from "./views/screens/splash";
+import Main from "./views/screens/Main";
+import Login from "./views/screens/Login";
 
 const Stack = createStackNavigator();
 
-const App = () => {
-  return (
+const App= () => {
+  return(
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Splash">
         <Stack.Screen name="Splash" component={Splash} />
         <Stack.Screen name="Main" component={Main} />
         <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen
-          name="Portfolio"
-          options={{headerShown: false}}
-          component={Portfolio}
-        />
       </Stack.Navigator>
     </NavigationContainer>
-  );
+  )
 };
 
 export default App;
--- views/screens/Login.js
+++ views/screens/Login.js
@@ -1,23 +1,32 @@
 import React from 'react';
-import {StyleSheet, Button, View} from 'react-native';
+import {
+StyleSheet,
+View,
+} from 'react-native';
 import Input from '../components/Input';
 
-const Login = ({navigation}) => {
-  return (
+
+
+const Login= () => {
+return(
     <View style={styles.container}>
-      <Input inputType={'id'} placeholder={'아이디'} />
-      <Input inputType={'password'} placeholder={'비밀번호'} />
-      <Button
-        title="로그인"
-        onPress={() => navigation.navigate('Portfolio')}></Button>
+        <Input
+        inputType={'id'}
+        placeholder={'아이디'}
+        />
+        <Input
+        inputType={'password'}
+        placeholder={'비밀번호'}
+        />
     </View>
-  );
+)
 };
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: 'white',
-  },
+    container:{
+    backgroundColor: 'white'
+    }
 });
+
 
 export default Login;
