bd04b36bc6a96a6d0d6fed1a7b791a24b10e7280
BecomePro <yesrjsdn@gmail.com>
2023-08-05 14:31:41 +0900

    공유 페이지 - 완료

--- views/components/ShareEditMode.js
+++ views/components/ShareEditMode.js
@@ -8,15 +8,11 @@ import {
   View,
   Image,
 } from 'react-native';
-import {launchImageLibrary} from 'react-native-image-picker';
 
-import DetailPop from '../screens/Portfolio/DetailPop';
-import ChoosePop from '../components/ChoosePop';
 import Title from '../components/Title';
 import cancelIcon from '../../assets/images/cancelIcon.png';
 import linkIcon from '../../assets/images/linkIcon.png';
 import kakaoIcon from '../../assets/images/kakaoIcon.png';
-import editingIcon from '../../assets/images/editingIcon.png';
 
 const styles = StyleSheet.create({
   modalBackdropPress: {
@@ -46,7 +42,7 @@ const styles = StyleSheet.create({
   },
 });
 
-const EditMode = ({
+const shareEditMode = ({
   //공통 프롭스
   isModalVisible,
   setIsModalVisible,
@@ -67,120 +63,16 @@ const EditMode = ({
   const [detailPopVisible, setDetailPopVisible] = useState(false);
   const [removeChoosePopVisible, setRemoveChoosePopVisible] = useState(false);
   const [saveChoosePopVisible, setSaveChoosePopVisible] = useState(false);
-  const [togleButton, setTogleButton] = useState(false);
   const [checkDeletePopOkButton, setCheckDeletePopOkButton] = useState(false); //삭제 츄즈 팝업에서 확인을 눌렀는지 감지하여 네비바의 저장을 눌렀을 시 ChoosePop이 뜨도록 함
   const [checkChoosePopOkButton, setCheckChoosePopOkButton] = useState(false); //디테일 팝업에서 확인을 눌렀는지 감지하여 네비바의 저장을 눌렀을 시 ChoosePop이 뜨도록 함
-  //인터뷰 스테이트
-  const [changeData, setChangeData] = useState(null); // 수정 전 변경 내용 임시 저장
-  const [deletePopVisible, setDeletePopVisible] = useState(false); // 삭제 확인 창 상태
-  const [savePopVisible, setSavePopVisible] = useState(false);
-  const [isEditing, setIsEditing] = useState(false); // 수정 여부 확인 ( 수정 내용 없으면 저장 버튼 뜨지 않도록)
-  const [changeHeart, setChangeHeart] = useState(heart); // 수정 내용 저장 전 하트 변경 사항 저장
-  const [deleteAlertVisible, setDeleteAlertVisible] = useState(false);
-
-  //인터뷰 컴포넌트 start-----------------------------------------------------------------------------------------------------------------------------------------
-  // 갤러리에서 video 파일 선택
-  const chooseFile = type => {
-    let options = {
-      mediaType: 'video',
-      maxWidth: 300,
-      maxHeight: 550,
-      videoQuality: 'low',
-    };
-    launchImageLibrary(options, response => {
-      if (response === false) {
-        // 선택한 이미지가 없는 경우
-        console.log('User did not select an image');
-        return;
-      }
-      console.log('Response = ', response);
-
-      if (response.assets && response.assets.length > 0) {
-        const asset = response['assets'][0];
-        console.log('base64 -> ', asset.base64);
-        console.log('uri -> ', asset.uri);
-        console.log('width -> ', asset.width);
-        console.log('height -> ', asset.height);
-        console.log('fileSize -> ', asset.fileSize);
-        console.log('type -> ', asset.type);
-        console.log('fileName -> ', asset.fileName);
-
-        if (checkDeletePopOkButton == false) setChangeData(asset.uri);
-        setTogleButton(true);
-      }
-    });
-  };
-
-  //인터뷰 세이브
-  const handleSave = () => {
-    //setTogleButton(true);
-    setIsPlaying(true);
-    setFilePath(changeData);
-    setChangeData(); // 저장된 데이터 초기화
-    // setSavePopVisible(false);
-    setHeart(changeHeart);
-  };
-
-  // 인터뷰 딜리트
-  const deleteUrl = () => {
-    setChangeData(false);
-  };
-  //인터뷰 컴포넌트 end-----------------------------------------------------------------------------------------------------------------------------------------
 
   // EditMode Button onPress 용 Props 컴포넌트 start------------------------------------------------------------------------------------------------------------------------
-  const editingButton = () => {
-    //수정 섹션을 클릭한 경우
-    //인터뷰: interview 스테이트를 통해 인터뷰 페이지에서 진입했다면 하단 파일 탐색기 실행
-    if (interview == true) {
-      chooseFile('video');
-      console.log(checkDeletePopOkButton);
-    }
-    //포트폴리오: portfolio 스테이트를 통해 포트폴리오 페이지에서 진입했다면 하단 디테일 팝 실행
-    else if (portfolio == true) {
-      setCheckChoosePopOkButton(false); //수정 누를 때 디테일 팝 확인버튼 클릭 여부 스테이트 초기화
-      setDetailPopVisible(true);
-      setTogleButton(true);
-    }
+  const copyLinkButton = () => {
+    alert('링크가 클립보드에 복사되었습니다.');
   };
 
-  const saveButton = () => {
-    //저장 섹션을 클릭한 경우
-    //인터뷰: interview 스테이트를 통해 인터뷰 페이지에서 진입했다면 하단 파일 탐색기 실행
-    if (interview == true) {
-      setSaveChoosePopVisible(true);
-    }
-    //포트폴리오 portfolio 스테이트를 통해 포트폴리오 페이지에서 진입했다면 하단 디테일 팝 실행
-    else if (portfolio == true) {
-      if (checkChoosePopOkButton == false) {
-        console.log('if (checkChoosePopOkButton === false) 디테일팝업 취소');
-        if (checkDeletePopOkButton == true) {
-          setDetailPopVisible(false);
-          setSaveChoosePopVisible(true); // 그러나 삭제 츄즈 팝업에서 확인을 눌렀다면 디테일 팝업 닫고 저장 클릭 시 츄즈팝업 실행
-        } else if (checkDeletePopOkButton == false) {
-          setDetailPopVisible(true);
-          //디테일 팝업에서 취소 누른 경우 저장 클릭 시 디테일 팝업만 계속 실행
-        }
-      } else if (checkChoosePopOkButton == true) {
-        setDetailPopVisible(false);
-        console.log(
-          'else if (checkChoosePopOkButton === true) 디테일팝업 확인',
-        );
-        setSaveChoosePopVisible(true); // 디테일 팝업에서 확인 누른 경우 이후에 저장 클릭 시 디테일 팝업 대신 저장 츄즈 팝업 실행
-      }
-    }
-  };
-  const deletedButton = () => {
-    //인터뷰: interview 스테이트를 통해 인터뷰 페이지에서 진입했다면 하단 파일 탐색기 실행
-    if (interview == true) {
-      setRemoveChoosePopVisible(true);
-      setTogleButton(true);
-    }
-    //포트폴리오 portfolio 스테이트를 통해 포트폴리오 페이지에서 진입했다면 하단 디테일 팝 실행
-    else if (portfolio == true) {
-      setRemoveChoosePopVisible(true);
-      setTogleButton(true);
-    }
-    // modalView: 모달 영역 안 (Modify, Delete 기능이 담긴 Bottom Nav(Modal) 생성)
+  const shareKakaoButton = () => {
+    alert('카카오톡 공유로 이동');
   };
 
   // EditMode Button onPress 용 Props 컴포넌트 end------------------------------------------------------------------------------------------------------------------------
@@ -207,7 +99,6 @@ const EditMode = ({
       <TouchableOpacity
         onPress={() => {
           setIsModalVisible(false);
-          setTogleButton(false);
         }} // modalBackdropPress: 모달 영역 밖 클릭 시 Bottom Nav(Modal) 닫힘 구현을 위해 TouchableOpacity로 modalView를 감싸서 적용
         style={styles.modalBackdropPress}>
         <Pressable
@@ -215,82 +106,29 @@ const EditMode = ({
             setIsModalVisible(true);
           }} // Pressable: 모달 영역 안 클릭 시 Bottom Nav(Modal) 유지 구현을 위해 Pressable로 감싸서 적용
           style={[styles.modalView, styles.shadow]}>
-          {togleButton === false && (
-            <EditModeSectionChooseBtn
-              title={'링크 복사'}
-              source={linkIcon}
-              onPress={() => {
-                editingButton();
-              }}></EditModeSectionChooseBtn>
-          )}
-          {togleButton === true && (
-            <EditModeSectionChooseBtn
-              title={'저장'}
-              source={saveIcon}
-              onPress={() => {
-                saveButton();
-              }}></EditModeSectionChooseBtn>
-          )}
+          <EditModeSectionChooseBtn
+            title={'링크 복사'}
+            source={linkIcon}
+            onPress={() => {
+              copyLinkButton();
+            }}></EditModeSectionChooseBtn>
+
           <EditModeSectionChooseBtn
             title={'카카오톡 공유'}
             source={kakaoIcon}
             onPress={() => {
-              deletedButton();
+              shareKakaoButton();
             }}></EditModeSectionChooseBtn>
+
           <EditModeSectionChooseBtn
             title={'취소'}
             source={cancelIcon}
             onPress={() => {
               setIsModalVisible(!isModalVisible);
-              setTogleButton(false);
             }}></EditModeSectionChooseBtn>
-
-          <DetailPop
-            id={id}
-            onModify={onModify}
-            checkChoosePopOkButton={checkChoosePopOkButton}
-            setCheckChoosePopOkButton={setCheckChoosePopOkButton}
-            detailPopVisible={detailPopVisible}
-            setTogleButton={setTogleButton}
-            setDetailPopVisible={setDetailPopVisible}></DetailPop>
-          <ChoosePop
-            //공통
-            title="수정된 내용을 저장하시겠습니까?"
-            setTogleButton={setTogleButton}
-            checkDeletePopOkButton={checkDeletePopOkButton}
-            setCheckDeletePopOkButton={setCheckDeletePopOkButton}
-            setCheckChoosePopOkButton={setCheckChoosePopOkButton}
-            setIsModalVisible={setIsModalVisible}
-            choosePopVisible={saveChoosePopVisible}
-            setChoosePopVisible={setSaveChoosePopVisible}
-            //인터뷰
-            interview={interview}
-            handleSave={handleSave}
-            deleteUrl={deleteUrl}
-            //포트폴리오
-            portfolio={portfolio}
-            id={id}
-            onModify={onModify}
-            onDelete={onDelete}></ChoosePop>
-          <ChoosePop
-            //공통
-            title="수정된 내용을 삭제하시겠습니까?"
-            checkDeletePopOkButton={checkDeletePopOkButton}
-            setCheckDeletePopOkButton={setCheckDeletePopOkButton}
-            setIsModalVisible={setIsModalVisible}
-            isModalVisible={isModalVisible}
-            choosePopVisible={removeChoosePopVisible}
-            setChoosePopVisible={setRemoveChoosePopVisible}
-            //인터뷰
-            setChangeData={setChangeData}
-            setChangeHeart={setChangeHeart}
-            setIsEditing={setIsEditing}
-            //포트폴리오
-            portfolio={portfolio}
-            id={id}></ChoosePop>
         </Pressable>
       </TouchableOpacity>
     </Modal>
   );
 };
-export default EditMode;
+export default shareEditMode;
--- views/screens/Share.js
+++ views/screens/Share.js
@@ -1,3 +1,195 @@
+// import React, {useState, useEffect, useRef} from 'react';
+// import {
+//   View,
+//   Text,
+//   StyleSheet,
+//   Image,
+//   TouchableOpacity,
+//   SafeAreaView,
+//   ScrollView,
+//   Modal,
+//   TouchableWithoutFeedback,
+//   Dimensions,
+// } from 'react-native';
+// import ShareEditMode from '../components/ShareEditMode';
+
+// const shareImages = [
+//   {
+//     id: 1,
+//     url: require('../../assets/images/test_member1.jpeg'),
+//   },
+//   {
+//     id: 2,
+//     url: require('../../assets/images/test_member2.jpeg'),
+//   },
+//   {
+//     id: 3,
+//     url: require('../../assets/images/test_member3.jpeg'),
+//   },
+// ];
+
+// const shareImages2 = [
+//   {
+//     id: 4,
+//     url: require('../../assets/images/test_member4.jpeg'),
+//   },
+//   {
+//     id: 5,
+//     url: require('../../assets/images/test_member1.jpeg'),
+//   },
+//   {
+//     id: 6,
+//     url: require('../../assets/images/test_member2.jpeg'),
+//   },
+// ];
+
+// const Share = () => {
+//   const [isModalVisible, setIsModalVisible] = useState(false);
+
+//   const handleCloseModal = () => {
+//     setSelectedImage(null);
+//   };
+
+//   const closeButtonRef = useRef(null);
+
+//   return (
+//     <SafeAreaView style={styles.container}>
+//       <ScrollView>
+//         <View style={styles.header}>
+//           <Text style={styles.headerTitle}>마스외전 2023년</Text>
+//           <Text style={styles.headerSubTitle}>111개</Text>
+//         </View>
+//         <View style={styles.divider} />
+//         <View style={styles.body}>
+//           <View style={styles.bodyContent}>
+//             <View style={styles.imageContainer}>
+//               {shareImages.map(image => (
+//                 <TouchableOpacity
+//                   key={image.id}
+//                   onPress={() => setIsModalVisible(true)}>
+//                   <Image
+//                     style={styles.imageContent}
+//                     source={image.url}
+//                     resizeMode="cover"
+//                   />
+//                 </TouchableOpacity>
+//               ))}
+//             </View>
+//             <View style={styles.imageContainer}>
+//               {shareImages2.map(image => (
+//                 <TouchableOpacity
+//                   key={image.id}
+//                   onPress={() => setIsModalVisible(true)}>
+//                   <Image
+//                     style={styles.imageContent}
+//                     source={image.url}
+//                     resizeMode="cover"
+//                   />
+//                 </TouchableOpacity>
+//               ))}
+//             </View>
+//           </View>
+//         </View>
+
+//         <View style={styles.header}>
+//           <Text style={styles.headerTitle}>마스외전 2022년</Text>
+//           <Text style={styles.headerSubTitle}>111개</Text>
+//         </View>
+//         <View style={styles.divider} />
+//         <View style={styles.body}>
+//           <View style={styles.bodyContent}>
+//             <View style={styles.imageContainer}>
+//               {shareImages.map(image => (
+//                 <TouchableOpacity
+//                   key={image.id}
+//                   onPress={() => setIsModalVisible(true)}>
+//                   <Image
+//                     style={styles.imageContent}
+//                     source={image.url}
+//                     resizeMode="cover"
+//                   />
+//                 </TouchableOpacity>
+//               ))}
+//             </View>
+//             <View style={styles.imageContainer}>
+//               {shareImages2.map(image => (
+//                 <TouchableOpacity
+//                   key={image.id}
+//                   onPress={() => setIsModalVisible(true)}>
+//                   <Image
+//                     style={styles.imageContent}
+//                     source={image.url}
+//                     resizeMode="cover"
+//                   />
+//                 </TouchableOpacity>
+//               ))}
+//             </View>
+//           </View>
+//         </View>
+//         <ShareEditMode
+//           isModalVisible={isModalVisible}
+//           setIsModalVisible={setIsModalVisible}></ShareEditMode>
+//       </ScrollView>
+//     </SafeAreaView>
+//   );
+// };
+
+// export default Share;
+
+// const {width} = Dimensions.get('window').width;
+// const {height} = Dimensions.get('window').height;
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     width: width,
+//     height: height,
+//     backgroundColor: 'white',
+//   },
+//   header: {
+//     marginTop: 20,
+//     marginHorizontal: 20,
+//     height: 30,
+//     flexDirection: 'row',
+//     justifyContent: 'space-between',
+//   },
+//   headerTitle: {
+//     fontSize: 20,
+//     fontWeight: 'bold',
+//     color: '#333333',
+//   },
+//   headerSubTitle: {
+//     position: 'absolute',
+//     bottom: 0,
+//     right: 0,
+//     fontSize: 12,
+//     fontWeight: 'normal',
+//     color: '#333333',
+//   },
+//   body: {
+//     marginHorizontal: 20,
+//   },
+//   bodyContent: {
+//     flexDirection: 'column',
+//     alignItems: 'center',
+//   },
+//   imageContainer: {
+//     flexDirection: 'row',
+//   },
+//   imageContent: {
+//     width: 105,
+//     height: 105,
+//     margin: 8,
+//   },
+//   divider: {
+//     width: width,
+//     height: 1,
+//     marginVertical: 10,
+//     marginHorizontal: 20,
+//     backgroundColor: '#D8D8D8',
+//   },
+// });
+
 import React, {useState, useEffect, useRef} from 'react';
 import {
   View,
@@ -43,14 +235,8 @@ const shareImages2 = [
   },
 ];
 
-const Share = () => {
+const Album = () => {
   const [isModalVisible, setIsModalVisible] = useState(false);
-
-  const handleCloseModal = () => {
-    setSelectedImage(null);
-  };
-
-  const closeButtonRef = useRef(null);
 
   return (
     <SafeAreaView style={styles.container}>
@@ -62,9 +248,10 @@ const Share = () => {
         <View style={styles.divider} />
         <View style={styles.body}>
           <View style={styles.bodyContent}>
-            <View style={styles.imageContainer}>
+            <View style={styles.imageRowContainer}>
               {shareImages.map(image => (
                 <TouchableOpacity
+                  style={styles.imageContainer}
                   key={image.id}
                   onPress={() => setIsModalVisible(true)}>
                   <Image
@@ -75,9 +262,10 @@ const Share = () => {
                 </TouchableOpacity>
               ))}
             </View>
-            <View style={styles.imageContainer}>
+            <View style={styles.imageRowContainer}>
               {shareImages2.map(image => (
                 <TouchableOpacity
+                  style={styles.imageContainer}
                   key={image.id}
                   onPress={() => setIsModalVisible(true)}>
                   <Image
@@ -98,9 +286,10 @@ const Share = () => {
         <View style={styles.divider} />
         <View style={styles.body}>
           <View style={styles.bodyContent}>
-            <View style={styles.imageContainer}>
+            <View style={styles.imageRowContainer}>
               {shareImages.map(image => (
                 <TouchableOpacity
+                  style={styles.imageContainer}
                   key={image.id}
                   onPress={() => setIsModalVisible(true)}>
                   <Image
@@ -111,9 +300,10 @@ const Share = () => {
                 </TouchableOpacity>
               ))}
             </View>
-            <View style={styles.imageContainer}>
+            <View style={styles.imageRowContainer}>
               {shareImages2.map(image => (
                 <TouchableOpacity
+                  style={styles.imageContainer}
                   key={image.id}
                   onPress={() => setIsModalVisible(true)}>
                   <Image
@@ -134,10 +324,9 @@ const Share = () => {
   );
 };
 
-export default Share;
+export default Album;
 
-const {width} = Dimensions.get('window').width;
-const {height} = Dimensions.get('window').height;
+const {width, height} = Dimensions.get('window');
 
 const styles = StyleSheet.create({
   container: {
@@ -173,19 +362,24 @@ const styles = StyleSheet.create({
     flexDirection: 'column',
     alignItems: 'center',
   },
-  imageContainer: {
+  imageRowContainer: {
     flexDirection: 'row',
   },
   imageContent: {
-    width: 105,
-    height: 105,
+    width: '100%',
+    height: '100%',
+    borderRadius: 10,
+  },
+  imageContainer: {
+    width: width * 0.27,
+    height: height * 0.15,
     margin: 8,
+    elevation: 8,
   },
   divider: {
-    width: width,
-    height: 1,
-    marginVertical: 10,
     marginHorizontal: 20,
-    backgroundColor: '#D8D8D8',
+    marginVertical: 10,
+    borderBottomColor: '#E5E5E5',
+    borderBottomWidth: 1,
   },
 });
