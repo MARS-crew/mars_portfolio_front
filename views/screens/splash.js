2df7a396190b1d3975c0c97bf7b42bdff2b52a1f
nnnnnnniam1 <nnnnnnniam1@gmail.com>
2023-08-09 22:25:25 +0900

    .

--- views/screens/splash.js
+++ views/screens/splash.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import {View, Image, StyleSheet, TouchableOpacity, Text} from 'react-native';
+import { View, Image, StyleSheet, TouchableOpacity, Text } from 'react-native';
 import LottieView from 'lottie-react-native';
 
-const Splash = ({navigation}) => {
+const Splash = ({ navigation }) => {
     return (
-    <View>
-    <Text>그림을 클릭시 메인화면으로 갑니다.</Text>
-    <TouchableOpacity onPress={() => navigation.navigate('Main')}>
-        <LottieView
-        source={require('../../assets/lottie/spaceship.json')}
-        style={styles.lottie}
-        autoPlay
-        loop
-        />
-    </TouchableOpacity>
-    </View>
-);
+        <View>
+            <Text>그림을 클릭시 메인화면으로 갑니다.</Text>
+            <TouchableOpacity onPress={() => navigation.navigate('Main')}>
+                <LottieView
+                    source={require('../../assets/lottie/spaceship.json')}
+                    style={styles.lottie}
+                    autoPlay
+                    loop
+                />
+            </TouchableOpacity>
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
