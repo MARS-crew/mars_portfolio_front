5a93dc765ec8f1442d3e3d5ed0ad340b9d3307b2
nnnnnnniam1 <nnnnnnniam1@gmail.com>
2023-08-28 03:41:41 +0900

    스와이프 시 인덱스 변경

--- App.js
+++ App.js
@@ -1,120 +1,21 @@
 import React, { useState, useEffect } from 'react';
-import {
-  Animated,
-  Dimensions,
-  ScrollView,
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
-
+import { Animated, View, StyleSheet, ScrollView, Text } from 'react-native';
 import Splash from './views/screens/splash';
-import Login from './views/screens/Login';
-import Group from './views/screens/Group';
-import Member from './views/screens/Member';
-import Youtube from './views/screens/Youtube';
-import Resume from './views/screens/Resume';
-import Portfolio from './views/screens/Portfolio/Portfolio';
-import Review from './views/screens/Review';
-import MyPage from './views/screens/MyPage/MyPage';
-import Album from './views/screens/Album';
-import Interview from './views/screens/Interview';
-import InterviewContents from './views/screens/InterviewContents';
-import 'react-native-gesture-handler';
-import WhichGroup from './views/screens/WhichGroup';
-import Help from './views/screens/Help';
-import Share from './views/screens/Share';
-
-import GroupVideo from './views/screens/GroupVideo';
+import Swiper from 'react-native-swiper';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import Swiper from 'react-native-swiper';
 
-const SCREEN_WIDTH = Dimensions.get('window').width;
-const SCREEN_HEIGHT = Dimensions.get('window').height;
 
-const Stack = createStackNavigator();
+import Group1 from './Group1';
+import Group2 from './Group2';
 
 const App = () => {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Help"
-          component={Help}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Share"
-          component={Share}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Album"
-          component={Album}
-          options={{ headerShown: false }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-};
 
-const datas = [
-  {
-    id: 1,
-    group: 3,
-    interviewData: '',
-    portfolioData: '',
-    resumeData: '',
-    reviewData: '',
-  },
-  {
-    id: 2,
-    group: 3,
-    interviewData: '',
-    portfolioData: '',
-    resumeData: '',
-    reviewData: '',
-  },
-  {
-    id: 3,
-    group: 4,
-    interviewData: '',
-    portfolioData: '',
-    resumeData: '',
-    reviewData: '',
-  },
-  {
-    id: 4,
-    group: 4,
-    interviewData: '',
-    portfolioData: '',
-    resumeData: '',
-    reviewData: '',
-  },
-  {
-    id: 5,
-    group: 3,
-    interviewData: '',
-    portfolioData: '',
-    resumeData: '',
-    reviewData: '',
-  },
-];
-
-const HomeScreen = () => {
-  const [modalOpen, setModalOpen] = useState(false);
   const [isSplashVisible, setIsSplashVisible] = useState(true);
   const [currentPage, setCurrentPage] = useState(0);
   const [currentIndex, setCurrentIndex] = useState(0);
-  const [isPlaying, setIsPlaying] = useState(false);
+  const [subIndex, setSubIndex] = useState(0);
 
   useEffect(() => {
     AsyncStorage.getItem('isSplashVisible').then(value => {
@@ -135,61 +36,52 @@ const HomeScreen = () => {
   }, [isSplashVisible]);
 
   return (
-    <View style={styles.container}>
-      <Swiper style={styles.wrapper} showsPagination={false}>
-        <View style={styles.slide}>
-          <Splash isSplashVisible={isSplashVisible} />
-          {isSplashVisible === false ? <WhichGroup index={1} /> : null}
-        </View>
-        <GroupVideo index={1} />
-        <InterviewContents
-          index={2}
-          path={''}
-          currentPage={currentPage}
-          currentIndex={currentIndex}
-          isPlaying={currentIndex === 2 && isPlaying}
-          setIsPlaying={setIsPlaying}
-        />
-        <Resume modalOpen={modalOpen} index={3} />
-        <Portfolio options={{ headerShown: false }} index={4} />
-        <Review index={5} />
-        <View style={styles.slide}>
-          <MyPage options={{ headerShown: false }} index={6} />
-        </View>
-      </Swiper>
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Splash isSplashVisible={isSplashVisible} />
+        {isSplashVisible === false ? (
+          <Swiper
+            horizontal={false}
+            showsPagination={false}
+            loop={false}
+          // index={currentIndex}
+          //onIndexChanged={(subIndex) => setCurrentIndex(subIndex)}
+          >
+            <View style={styles.slide}>
+              <Group1
+                currentPage={currentPage}
+                setCurrentPage={setCurrentPage}
+                currentIndex={currentIndex}
+                setCurrentIndex={setCurrentIndex}
+                subIndex={subIndex}
+                setSubIndex={setSubIndex}
+              />
+            </View>
+            <View style={styles.slide}>
+              <Group2
+                currentPage={currentPage}
+                setCurrentPage={setCurrentPage}
+                currentIndex={currentIndex}
+                setCurrentIndex={setCurrentIndex}
+                subIndex={subIndex}
+                setSubIndex={setSubIndex}
+              />
+            </View>
+          </Swiper>
+        ) : null}
+      </View>
+    </NavigationContainer>
   );
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    flexDirection: 'row',
-    backgroundColor: 'black',
-  },
-  scrollPage: {
-    width: SCREEN_WIDTH,
-  },
-  screen: {
-    width: '100%',
-    height: '100%',
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 0,
-    backgroundColor: 'white',
-  },
-  text: {
-    fontSize: 45,
-    fontWeight: 'bold',
-  },
-  wrapper: {
-    // backgroundColor: '#f00'
+  container: {
+    flex: 1,
   },
   slide: {
     flex: 1,
-    backgroundColor: 'transparent',
-  },
-  container: {
-    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
---
+++ Group2.js
@@ -0,0 +1,107 @@
+import React, { useState, useEffect } from 'react';
+import {
+  Animated,
+  Dimensions,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+import Swiper from 'react-native-swiper';
+
+import Splash from './views/screens/splash';
+import Resume from './views/screens/Resume';
+import Portfolio from './views/screens/Portfolio/Portfolio';
+import Review from './views/screens/Review';
+import MyPage from './views/screens/MyPage/MyPage';
+import Album from './views/screens/Album';
+import Interview from './views/screens/Interview';
+import 'react-native-gesture-handler';
+import WhichGroup2 from './views/screens/WhichGroup2';
+import Help from './views/screens/Help';
+import Share from './views/screens/Share';
+
+import GroupVideo from './views/screens/GroupVideo';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import InterviewContents from './views/screens/InterviewContents';
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
+
+const Group2 = ({
+  currentPage,
+  setCurrentPage,
+  currentIndex,
+  setCurrentIndex,
+  subIndex,
+  setSubIndex,
+}) => {
+  const [isPlaying, setIsPlaying] = useState(false);
+  return (
+    <Swiper
+      style={styles.wrapper}
+      showsPagination={false}
+      index={currentIndex}
+      loop={false}
+      onIndexChanged={index => setCurrentIndex(index)}
+    >
+      <WhichGroup2 index={0} />
+      <View style={styles.slide}>
+        <GroupVideo index={1} />
+      </View>
+      <View style={styles.slide}>
+        <InterviewContents
+          index={2}
+          path={''}
+          currentPage={currentPage}
+          currentIndex={currentIndex}
+          isPlaying={currentIndex === 2 && isPlaying}
+          setIsPlaying={setIsPlaying}
+        />
+      </View>
+      <View style={styles.slide}>
+        <Resume index={3} />
+      </View>
+      <View style={styles.slide}>
+        <Portfolio options={{ headerShown: false }} index={4} />
+      </View>
+      <View style={styles.slide}>
+        <Review index={5} />
+      </View>
+      <View style={styles.slide}>
+        <MyPage index={6} />
+      </View>
+    </Swiper>
+  );
+};
+
+const styles = StyleSheet.create({
+  scrollView: {
+    flexDirection: 'row',
+    backgroundColor: 'black',
+  },
+  scrollPage: {
+    width: SCREEN_WIDTH,
+  },
+  screen: {
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 0,
+    backgroundColor: 'white',
+  },
+  slide: {
+    flex: 1,
+    backgroundColor: 'transparent',
+  },
+  text: {
+    fontSize: 45,
+    fontWeight: 'bold',
+  },
+});
+
+export default Group2;
