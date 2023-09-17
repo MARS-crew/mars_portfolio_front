2d2925af1ff25a557578348e58de2ebd95d96b04
HoYeon Jo <akftjd100@gmail.com>
2023-09-17 17:00:51 +0900

    [수정] 마이페이지 중첩 스와이퍼 오류 해결, 토글 변경

--- views/screens/MyPage/LogList.js
+++ views/screens/MyPage/LogList.js
@@ -37,39 +37,164 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
+  log: {
+    paddingHorizontal: 15,
+  },
+  list: {
+    height: 41,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    borderBottomColor: '#F5F5F5',
+    borderBottomWidth: 1,
+    backgroundColor: '#FDFDFD',
+  },
+  item: {paddingVertical: 12, paddingHorizontal: 15},
+  deleteButton: {
+    backgroundColor: '#FFE9EA',
+    width: 70,
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 });
 
-const LogList = ListData => {
-  const [text, setText] = useState('Not Pressed');
-  const [myListData, setMyListData] = useState(ListData.ListData);
+const LogList = () => {
+  const [data, setData] = useState([
+    {
+      key: 1,
+      text: `이세진님이 회원님을 방문하였습니다.`,
+      date: `2023.11.01`,
+      showDelete: false,
+    },
+    {
+      key: 2,
+      text: `고희주님이 회원님을 방문하였습니다.`,
+      date: `2023.11.02`,
+      showDelete: false,
+    },
+    {
+      key: 3,
+      text: `이화진님이 회원님을 방문하였습니다.`,
+      date: `2023.11.03`,
+      showDelete: false,
+    },
+    {
+      key: 4,
+      text: `임동현님이 회원님을 방문하였습니다.`,
+      date: `2023.11.04`,
+      showDelete: false,
+    },
+    {
+      key: 5,
+      text: `이가인님이 회원님을 방문하였습니다.`,
+      date: `2023.11.05`,
+      showDelete: false,
+    },
+    {
+      key: 6,
+      text: `김건우님이 회원님을 방문하였습니다.`,
+      date: `2023.11.06`,
+      showDelete: false,
+    },
+    {
+      key: 7,
+      text: `장여운님이 회원님을 방문하였습니다.`,
+      date: `2023.11.07`,
+      showDelete: false,
+    },
+    {
+      key: 8,
+      text: `김채린님이 회원님을 방문하였습니다.`,
+      date: `2023.11.08`,
+      showDelete: false,
+    },
+    {
+      key: 9,
+      text: `이화진님이 회원님을 방문하였습니다.`,
+      date: `2023.11.09`,
+      showDelete: false,
+    },
+    {
+      key: 10,
+      text: `조호연님이 회원님을 방문하였습니다.`,
+      date: `2023.11.10`,
+      showDelete: false,
+    },
+    {
+      key: 11,
+      text: `벡예나님이 회원님을 방문하였습니다.`,
+      date: `2023.11.11`,
+      showDelete: false,
+    },
+    {
+      key: 12,
+      text: `문효찬님이 회원님을 방문하였습니다.`,
+      date: `2023.11.12`,
+      showDelete: false,
+    },
+    {
+      key: 13,
+      text: `박수민님이 회원님을 방문하였습니다.`,
+      date: `2023.11.13`,
+      showDelete: false,
+    },
+    {
+      key: 14,
+      text: `김인후님이 회원님을 방문하였습니다.`,
+      date: `2023.11.14`,
+      showDelete: false,
+    },
+    {
+      key: 15,
+      text: `김주만님이 회원님을 방문하였습니다.`,
+      date: `2023.11.15`,
+      showDelete: false,
+    },
+    {
+      key: 16,
+      text: `김회윤님이 회원님을 방문하였습니다.`,
+      date: `2023.11.16`,
+      showDelete: false,
+    },
+  ]);
+  const toggleDelete = key => {
+    setData(prevData =>
+      prevData.map(item =>
+        item.key === key ? {...item, showDelete: !item.showDelete} : item,
+      ),
+    );
+  };
 
   return (
-    <SafeAreaView style={styles.container}>
-      <SwipeListView
-        data={myListData}
+    <View style={styles.container}>
+      <FlatList
+        data={data}
         renderItem={({item}) => (
-          <View style={styles.swipeListItem}>
-            <Title color={'black'}>{item.text}</Title>
-            <Title>{item.date}</Title>
-          </View>
-        )}
-        renderHiddenItem={(data, rowMap) => (
-          <View style={styles.swipeHiddenItemContainer}>
-            <TouchableOpacity></TouchableOpacity>
-
-            <TouchableOpacity onPress={() => setText('right is pressed')}>
-              <View style={styles.swipeHiddenItem}>
-                <Title color={'#FF3040'} fontSize={16} fontWeight={'700'}>
-                  삭제
-                </Title>
-              </View>
+          <View style={styles.list}>
+            <TouchableOpacity
+              style={styles.item}
+              onPress={() => toggleDelete(item.key)}>
+              <Title color={'black'}>{item.text}</Title>
             </TouchableOpacity>
+            <View style={styles.log}>
+              {!item.showDelete && <Title>{item.date}</Title>}
+            </View>
+
+            {item.showDelete && (
+              <TouchableOpacity onPress={() => toggleDelete(item.key)}>
+                <View style={styles.deleteButton}>
+                  <Title color={'#FF3040'} fontSize={16} fontWeight={'700'}>
+                    삭제
+                  </Title>
+                </View>
+              </TouchableOpacity>
+            )}
           </View>
         )}
-        leftOpenValue={70}
-        rightOpenValue={-70}
+        keyExtractor={item => item.key.toString()}
       />
-    </SafeAreaView>
+    </View>
   );
 };
 export default LogList;
