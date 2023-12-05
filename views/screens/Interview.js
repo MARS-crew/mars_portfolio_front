fc89fd15ff32ae29a257d8cac5063817df628bde
MarieSJLEE <marie.sjlee@gmail.com>
2023-12-05 23:23:33 +0900

    찜하기 기능 완료

--- views/screens/Interview.js
+++ views/screens/Interview.js
@@ -17,66 +17,8 @@ const SCREEN_HEIGHT = Dimensions.get('window').height;
 const SCREEN_WIDTH = Dimensions.get('window').width;
 const yOffset = new Animated.Value(0);
 
-const Screen = props => {
-  return (
-    <View style={styles.scrollPage}>
-      <Animated.View style={[styles.screen, transitionAnimation(props.index)]}>
-        {props.children}
-      </Animated.View>
-    </View>
-  );
-};
-
-const Item = ({ id, path }) => (
-  <View>
-    <InterviewContents id={id} path={path} />
-  </View>
-);
-
-const transitionAnimation = index => {
-  return {
-    transform: [
-      { perspective: 800 },
-      {
-        scale: yOffset.interpolate({
-          inputRange: [
-            (index - 1) * SCREEN_HEIGHT,
-            index * SCREEN_HEIGHT,
-            (index + 1) * SCREEN_HEIGHT,
-          ],
-          outputRange: [1, 1, 1],
-        }),
-      },
-      {
-        translateY: yOffset.interpolate({
-          inputRange: [
-            (index - 1) * SCREEN_HEIGHT,
-            index * SCREEN_HEIGHT,
-            (index + 1) * SCREEN_HEIGHT,
-          ],
-          outputRange: [0, 0, 0],
-        }),
-      },
-    ],
-  };
-};
-
-const interviewFiles = [
-  {
-    id: 1,
-    path: '../../assets/videos/interviewVideo.mp4',
-  },
-  {
-    id: 2,
-    path: require('./interviewVideo.mp4'),
-  },
-  {
-    id: 3,
-    path: require('../../assets/video/mars_profil1.mp4'),
-  },
-];
-
 const Interview = ({ token }) => {
+
   const { currentIndex, changeIndex } = useIndexContext();
   const swiperRef = useRef(null);
 
@@ -104,7 +46,7 @@ const Interview = ({ token }) => {
     const source = axios.CancelToken.source();
     axios({
       method: 'get',
-      url: 'http://10.0.2.2:3000/api/v1/interview/',
+      url: 'http://192.168.0.2:3000/api/v1/interview/',
       headers: {
         Authorization: token
       },
@@ -112,19 +54,14 @@ const Interview = ({ token }) => {
     })
       .then(function (response) {
         const extractedData = response.data.data.map(item => ({
-          url: item.url,
+          memberId: item.member_id, //사용자 아이디
+          url: item.url, //인터뷰 url
+          heart: item.heart, //찜하기 여부
         }));
-        // setData(extractedData);
-
-        // console.log(extractedData);
-
-        const slicedData = extractedData.slice(1);
-
-        setData(slicedData);
-
-        console.log("interview--------------------------------------------------');");
-        console.log(slicedData);
-
+         setData(extractedData);
+         console.log('datadata::::'+extractedData[0].memberId);
+         console.log('datadata::::'+extractedData[0].url);
+         console.log('datadata::::'+extractedData[0].heart);
       })
       .catch(function (error) {
         console.log(error);
@@ -137,24 +74,12 @@ const Interview = ({ token }) => {
   }, []);
 
   return (
-    // <Animated.ScrollView
-    //   ref={swiperRef}
-    //   scrollEventThrottle={16}
-    //   // onScroll={handleScroll}
-    //   pagingEnabled
-    //   style={styles.scrollView}>
-    //   {data.map((item, index) => (
-    //     <Animated.View key={index} index={index} style={[styles.screen, transitionAnimation(index)]}>
-    //       <InterviewContents path={item.url} />
-    //     </Animated.View>
-    //   ))}
-    // </Animated.ScrollView>
     <SafeAreaView style={styles.container}>
       <SwiperFlatList
         ref={swiperRef}
         vertical={true}
         data={data}
-        renderItem={({ item }) => <InterviewContents id={item.id} path={item.url} />}
+        renderItem={({ item }) => <InterviewContents id={item.memberId} path={item.url} token={token} />}
         index={currentIndex}
         onScroll={handleScroll}
         hideShadow={true}
