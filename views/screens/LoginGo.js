18f02926eb05bc162938cc42ba75f1c78400bd4c
HoYeon Jo <akftjd100@gmail.com>
2024-01-16 19:19:15 +0900

    [완료] QA 로그인 창 퍼블리싱 오류 완료

--- views/screens/LoginGo.js
+++ views/screens/LoginGo.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {
   Text,
   View,
@@ -11,50 +11,53 @@ import {
 import {useNavigation, CommonActions} from '@react-navigation/native';
 
 const LoginGo = ({token}) => {
-    const navigation = useNavigation(); // 로그인 페이지로 넘기기 위한 네비게이션 객체
-
-    useEffect(() => {
-      console.log(`loginItem token ${token}`);
-    }, [token]);
+  const navigation = useNavigation(); // 로그인 페이지로 넘기기 위한 네비게이션 객체
+  const image = `../../assets/images/main_group_Image.png`;
+  useEffect(() => {
+    console.log(`loginItem token ${token}`);
+  }, [token]);
   return (
-    <View>
+    <ImageBackground source={require(image)} style={styles.container}>
       <TouchableOpacity
-          style={styles.button}
-          onPress={() => {
-            navigation.navigate('Login');
-          }}>
-          <Text style={styles.text}>로그인</Text>
-          <Image
-            source={require('../../assets/images/LoginNextItem.png')}
-            style={styles.image}
-          />
-        </TouchableOpacity>
-    </View>
+        style={styles.button}
+        onPress={() => {
+          navigation.navigate('Login');
+        }}>
+        <Text style={styles.text}>로그인</Text>
+        <Image
+          source={require('../../assets/images/LoginNextItem.png')}
+          style={styles.image}
+        />
+      </TouchableOpacity>
+    </ImageBackground>
   );
 };
 
 const styles = StyleSheet.create({
-    itemArea: {
-      flex: 1,
-    },
-    button: {
-      width: 104,
-      height: 38,
-      alignItems: 'center',
-      justifyContent: 'center',
-      borderWidth: 2,
-      borderStyle: 'solid',
-      bottom: 25,
-      right: 20,
-      borderRadius: 24,
-      borderColor: 'black',
-      border: 2,
-      position: 'absolute',
-      flexDirection: 'row',
-    },
-    text: {
-      color: 'black',
-      fontSize: 16,
-    },
-  });
+  container: {
+    flex: 1,
+
+    width: Dimensions.get('window').width,
+  },
+  button: {
+    width: 104,
+    height: 38,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 2,
+    borderStyle: 'solid',
+    bottom: 25,
+    right: 20,
+    borderRadius: 24,
+    borderColor: 'black',
+    border: 2,
+    position: 'absolute',
+    flexDirection: 'row',
+    borderColor: 'white',
+  },
+  text: {
+    color: 'white',
+    fontSize: 16,
+  },
+});
 export default LoginGo;
